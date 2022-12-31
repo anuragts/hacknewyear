48ab967f427647a84cca039470e5bc40d020110c
Anurag30112003 <anuragsharma011011@gmail.com>
2023-01-01 04:55:04 +0530

    home page image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { Button } from "@nextui-org/react";
 import { NextPage } from "next";
 import Link from "next/link";
+import year from "../public/img/year.svg";
+
 
 const Home: NextPage = () => {
   const router = useRouter();
@@ -13,6 +14,7 @@ const Home: NextPage = () => {
           Transform your resolutions into reality with <br /> our easy-to-use
           platform .
         </div>
+        <Image src={year} alt="imgggg" className="float-right mt-[-15rem] mx-[5]"  />
         <div className="mt-[5rem]">
           <Link href={"/dashboard"} className="text-secondary bg-primary px-[4rem] py-[1rem] rounded-full  text-3xl ml-[15rem]" >
             Take Off
